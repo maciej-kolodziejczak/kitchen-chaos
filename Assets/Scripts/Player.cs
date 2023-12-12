b068c57faee0b67166c7bcccfa67945a7dc32592
Maciej Kolodziejczak <maciej.kolodziejczak-ext@hexagon.com>
2023-12-12 11:14:57 +0100

    Apply basic collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
@@ -13,17 +15,53 @@ public class Player : MonoBehaviour
     
     private void Update()
     {
-        var inputVector = Vector2.zero;
-        inputVector = playerInput.GetNormalizedInputVector();
+        var inputVector = playerInput.GetNormalizedInputVector();
         
         var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         IsWalking = moveDirection != Vector3.zero;
+
+        var currentPosition = transform.position;
+        var canMove = !Physics.CapsuleCast(currentPosition, currentPosition + Vector3.up * 2f, .7f, moveDirection,
+            moveSpeed * Time.deltaTime);
         
-        ApplyMove(inputVector, moveDirection);
+        // rotation is applied before movement as we want to face movement direction regardless of the collision, which would constrain the moveDirection to a single axis
         ApplyRotation(moveDirection);
+
+        // @todo refactor this 
+        if (canMove)
+        { 
+            ApplyMove(moveDirection);
+        }
+        else
+        {
+            // cast to check if we can move on x axis
+            var canMoveOnX = !Physics.CapsuleCast(currentPosition, currentPosition + Vector3.up * 2f, .7f,
+                new Vector3(moveDirection.x, 0, 0), moveSpeed * Time.deltaTime);
+            // cast to check if we can move on z axis
+            var canMoveOnZ = !Physics.CapsuleCast(currentPosition, currentPosition + Vector3.up * 2f, .7f,
+                new Vector3(0, 0, moveDirection.z), moveSpeed * Time.deltaTime);
+            
+            // if we can move on x axis, update inputVector accordingly
+            if (canMoveOnX)
+            {
+                inputVector = new Vector2(inputVector.x, 0).normalized;
+            } else if (canMoveOnZ)
+            {
+                inputVector = new Vector2(0, inputVector.y).normalized;
+            }
+            
+            // update moveDirection accordingly
+            moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+            
+            // if we can move on either axis, apply move
+            if (canMoveOnX || canMoveOnZ)
+            {
+                ApplyMove(moveDirection);
+            }
+        }
     }
 
-    private void ApplyMove(Vector2 inputVector, Vector3 moveDirection)
+    private void ApplyMove(Vector3 moveDirection)
     {
         transform.position += moveDirection * (Time.deltaTime * moveSpeed);
     }
