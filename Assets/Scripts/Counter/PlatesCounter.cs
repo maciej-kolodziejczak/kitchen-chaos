dddfb69e7cc01fc3a2f0919e12fd4375810ef5fa
Maciej Kołodziejczak <maciej.kolodziejczak@icloud.com>
2023-12-19 22:47:28 +0100

    Add spawning of plates (visual)

--- Assets/Scripts/Counter/PlatesCounter.cs
+++ Assets/Scripts/Counter/PlatesCounter.cs
@@ -1,37 +1,51 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using Product;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Counter
 {
+    [RequireComponent(typeof(ProductHandler))]
     public class PlatesCounter : BaseCounter
     {
         [SerializeField] private int maxPlates;
         [SerializeField] private float spawnInterval;
+        [SerializeField] private GameObject plateVisualPrefab;
         
-        private int _platesCount;
+        private ProductHandler _productHandler;
+        private readonly Stack<GameObject> _plates = new();
+        private const float PlateHeight = 0.1f;
 
         private void Awake()
         {
+            _productHandler = GetComponent<ProductHandler>();
+            
             StartCoroutine(SpawnPlates());
         }
 
         private IEnumerator SpawnPlates()
         {
-            while (_platesCount < maxPlates)
+            while (_plates.Count < maxPlates)
             {
                 yield return new WaitForSeconds(spawnInterval);
-                
-                _platesCount++;
-                Debug.Log("plate spawned");
+                SpawnPlate();
             }
+        }
+
+        private void SpawnPlate()
+        {
+            var plateVisual = Instantiate(plateVisualPrefab, _productHandler.ProductOrigin);
+            plateVisual.transform.localPosition = Vector3.up * (PlateHeight * _plates.Count);
+            
+            _plates.Push(plateVisual);
         }
 
         public override void Interact(ProductHandler invoker)
         {
-            Debug.Log("plate taken");
-            _platesCount--;
+            if (_plates.Count == 0) return;
+            
+            var plate = _plates.Pop();
+            Destroy(plate);
             
             StopAllCoroutines();
             StartCoroutine(SpawnPlates());
